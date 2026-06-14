53631f0147567665317a2c1d836480de1534c0e7
jogh3 <joeyboi866@gmail.com>
2026-06-14 20:05:33 +0100

    did a bit more frontent, focusing on spa design

--- public/script.js
+++ public/script.js
@@ -1,30 +1,64 @@
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('home').addEventListener('click', (event) => {
-    load_main();
+  switch (window.location.pathname) {
+    case "/":
+      load_main();
+      break;
+    case "/settings":
+      load_settings();
+      break;
+    default:
+      break;
+  }
+  document.getElementById("sidebar").addEventListener('click', (event) => {
+    switch (event.target.id) {
+      case "home":
+        load_main();
+        break;
+      case "settings":
+        load_settings();
+        break;
+      default:
+        break;
+    }
   })
-  document.getElementById('test').addEventListener('click', (event) => {
-    console.log("testy pressed");
-    const reqpayload = {
-      method: "POST"
-    };
-    fetch('/dothing', reqpayload);
-  })
-  document.getElementById('test2').addEventListener('click', (event) => {
-    console.log("test pressed");
-    const reqpayload = {
-      method: "POST"
-    };
-    fetch('/dothingtoo',reqpayload);
+  document.getElementById("main_body").addEventListener('click', (event) => {
+    switch (event.target.id) {
+      case "test":
+        console.log("testy pressed");
+        send_payload("POST",'/dothing');
+        break;
+      case "test2":
+      console.log("test pressed");
+      send_payload("POST",'/dothingtoo');
+      default:
+        break;
+    }
   })
 });
 
+function send_payload(inmethod, url){
+  const reqpayload = {
+    method: inmethod
+  };
+  fetch(url,reqpayload);
+}
+
 function load_main(){
-  // history.pushstate();
-  // window.localstorage();
+  console.log("changing to home");
+  history.pushState(null,"","/");
   main_body = document.getElementById("main_body");
   main_body.innerHTML = `
     <h1>in the works</h1>
       <button id="test">testy</button>
       <button id="test2">testy2</button>
+  `;
+}
+function load_settings() {
+  console.log("changing to settings");
+  history.pushState(null,"","/settings");
+  main_body = document.getElementById("main_body");
+  main_body.innerHTML = `
+    <h1> settings page, fucking hell this is long</h1>
+      <p>poopy</p>
   `;
 }
